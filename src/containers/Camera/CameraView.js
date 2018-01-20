511c6c3e2849f48d427b913c09fab9a860fb0811
Nadzir <nadz.muhd@gmail.com>
2018-01-21 04:15:59 +0800

    Camera: Detect Logo and Text, Filter Halal

--- src/containers/Camera/CameraView.js
+++ src/containers/Camera/CameraView.js
@@ -1,17 +1,11 @@
-import React, { Component } from 'react';
-import {
-  View,
-  Image,
-  StyleSheet,
-} from 'react-native';
-import { Tile } from 'react-native-elements';
-import { Button, Header, Card, Text } from 'react-native-elements';
 import Camera from 'react-native-camera';
-import { Dimensions, Platform } from 'react-native';
-
-const { width, height } = Dimensions.get('window');
-const screenHeight = width < height ? height : width;
-const screenWidth = width < height ? width : height;
+import { chain, uniq, sortBy, reverse } from 'lodash';
+import React, { Component } from 'react';
+import { View, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
+import { Button, List, ListItem } from 'react-native-elements';
+import { resizeImage, convertImageToBase64 } from '../../lib/imageUtil/index';
+import { getLabel } from '../../lib/imageProcessing';
+import { isHalal, halalCheckType } from '../../lib/halal';
 
 const styles = StyleSheet.create({
   container: {
@@ -52,46 +46,106 @@ const styles = StyleSheet.create({
 });
 
 export class CameraView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [],
+      loading: false,
+    };
+  }
+
   takePicture () {
+    if (this.state.loading) return;
     console.log('Capturing camera');
+    this.setState({ loading: true });
     this.camera.capture()
       .then((data) => {
-        console.log(data.path);
         console.log('resizing image');
         return resizeImage(data.path);
       }).then((resizedImage) => {
         console.log('converting to base64');
-        return convertImageToBase64(resizedImage);
+        return convertImageToBase64(resizedImage.path);
       }).then((image64) => {
         console.log('getting label');
         return getLabel(image64);
       })
       .then((data) => {
-        console.log(data);
+        const logos = data && data.responses && data.responses[0].logoAnnotations;
+        const text = data && data.responses && data.responses[0].textAnnotations;
+
+        const logosList = chain(logos || [])
+          .map(item => ({
+            key: item.description,
+            title: item.description,
+            boundingPoly: item.boundingPoly,
+            halal: isHalal(halalCheckType.TEXT, item.description) ? 'Halal' : 'Not halal',
+            type: 'Logo',
+          })).value();
+
+        const textList = chain(text || [])
+          .filter(item => item.description.length > 3 && item.description.length < 20)
+          .map(item => ({
+            key: item.description,
+            title: item.description,
+            boundingPoly: item.boundingPoly,
+            halal: isHalal(halalCheckType.TEXT, item.description) ? 'Halal' : 'Not halal',
+            type: 'Text',
+          })).value();
+
+        const list = chain(logosList.concat(textList))
+          .uniqBy('key')
+          .sortBy(['halal', 'type'])
+          .value();
+        console.log(list);
+        this.setState({ list, loading: false });
       })
       .catch(err => console.error(err));
   }
 
   render() {
     return (
-      <View style={{ flex: 1, flexDirection: 'column' }}>
+      <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-end' }}>
+
         <Camera
           ref={(cam) => {
             this.camera = cam;
           }}
           style={styles.preview}
           aspect={Camera.constants.Aspect.fill}
+          captureTarget={Camera.constants.CaptureTarget.disk}
         >
-          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]
-          </Text>
-          <Button
-            icon={{ name: 'code' }}
-            backgroundColor="#03A9F4"
-            fontFamily="Lato"
-            buttonStyle={{
-              borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0,
+          <ActivityIndicator size="large" color="#0000ff" animating={this.state.loading} />
+          <List
+            containerStyle={{
+              opacity: 0.9, height: 200, alignSelf: 'stretch',
             }}
-            title="Is it halal?"
+          >
+            <FlatList
+              data={this.state.list || []}
+              keyExtractor={item => item.key}
+              renderItem={({ item }) => (
+                <ListItem
+                  title={item.title}
+                  subtitle={`${item.halal} ( ${item.type} )`}
+                />
+              )}
+            />
+          </List>
+          <Button
+            backgroundColor={this.state.loading ? '#e74c3c' : '#2ecc71'}
+            buttonStyle={{
+              borderRadius: 0,
+              marginLeft: 0,
+              marginRight: 0,
+              marginBottom: 0,
+              height: 50,
+            }}
+            containerViewStyle={{
+              width: '100%',
+            }}
+            title="What is this?"
+            onPress={this.takePicture.bind(this)}
           />
         </Camera>
       </View>
