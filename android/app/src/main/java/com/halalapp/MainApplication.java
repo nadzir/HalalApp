4edbee54ca529b04649be4cac8ed226d58707940
Nadzir <nadz.muhd@gmail.com>
2018-05-05 06:12:05 +0800

    Add admob

--- android/app/src/main/java/com/halalapp/MainApplication.java
+++ android/app/src/main/java/com/halalapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.halalapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.sbugert.rnadmob.RNAdMobPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAdMobPackage(),
             new SvgPackage(),
             new VectorIconsPackage(),
             new ImageResizerPackage(),
